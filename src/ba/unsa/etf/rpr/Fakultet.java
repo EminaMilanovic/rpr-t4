5e35fec684ce63220220d3945f7a57b6b8646dfc
emilanovic1 <emilanovic1@etf.unsa.ba>
2018-11-06 03:44:06 +0100

    definisane metode i atributi

---
+++ src/ba/unsa/etf/rpr/Fakultet.java
@@ -0,0 +1,8 @@
+package ba.unsa.etf.rpr;
+
+public class Fakultet {
+    private Student []studenti;
+    private Predmet [] predmeti;
+    private String naziv;
+    public void upisiStudenta(Student s){}
+}
---
+++ src/ba/unsa/etf/rpr/Predmet.java
@@ -0,0 +1,18 @@
+package ba.unsa.etf.rpr;
+
+public class Predmet {
+    private String naziv;
+    private  Student[] studenti;
+    private boolean obavezan;
+    public void upisiStudentaNaPredmet(Student s){}
+    public String spisakStudenataNaPredmetu(){}
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+
+}
