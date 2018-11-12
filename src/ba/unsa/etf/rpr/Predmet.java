c04cd1f7a281f415ded07b00378685d484d0bcdb
emilanovic1 <emilanovic1@etf.unsa.ba>
2018-11-12 21:33:35 +0100

    popravila s obzirom na popravku diagrama

--- src/ba/unsa/etf/rpr/Predmet.java
+++ src/ba/unsa/etf/rpr/Predmet.java
@@ -3,15 +3,46 @@ package ba.unsa.etf.rpr;
 public class Predmet {
     private String naziv;
     private  Student[] studenti;
+    private int ects;
     private boolean obavezan;
+
+    public Predmet(String naziv, int ects, boolean obavezan) {
+        this.naziv = naziv;
+        this.ects = ects;
+        this.obavezan = obavezan;
+    }
+
+    public Predmet() {
+
+    }
+
+
+    public boolean isObavezan() {
+        return obavezan;
+    }
+
+    public void setObavezan(boolean obavezan) {
+        this.obavezan = obavezan;
+    }
+
+
+    public int getEcts() {
+        return ects;
+    }
+
+    public void setEcts(int ects) {
+        this.ects = ects;
+    }
+
+
     public void upisiStudentaNaPredmet(Student s){}
-    public String spisakStudenataNaPredmetu(){}
+    public String spisakStudenataNaPredmetu(){ return"";}
     public String getNaziv() {
         return naziv;
     }
-     /*dnasjknfoknojfpodsjpofjsdopjfopdsjofpsd*/
-    public void setNaziv(String naziv) {
-        this.naziv = naziv;
+
+    public void setNaziv(String n) {
+        this.naziv = n;
     }
 
 
--- src/ba/unsa/etf/rpr/Semestar.java
+++ src/ba/unsa/etf/rpr/Semestar.java
@@ -1,8 +1,16 @@
 package ba.unsa.etf.rpr;
 
-public class Fakultet {
+public class Semestar {
     private Student []studenti;
     private Predmet [] predmeti;
     private String naziv;
     public void upisiStudenta(Student s){}
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
 }
